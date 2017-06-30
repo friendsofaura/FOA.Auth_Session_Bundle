abca5a42b8d27524e9282357f08a34c2af861df5
Hari KT <kthari85@gmail.com>
2017-06-30 11:15:32 +0530

    Remove randomlib in favour of random_compact

--- tests/unit/src/SessionFactoryTest.php
+++ tests/unit/src/SessionFactoryTest.php
@@ -1,9 +1,6 @@
 <?php
 namespace FOA\Auth_Session_Bundle;
 
-use RandomLib\Factory as RandomLibFactory;
-use SecurityLib\Strength;
-
 class SessionFactoryTest extends \PHPUnit_Framework_TestCase
 {
     protected function setUp()
@@ -15,22 +12,5 @@ class SessionFactoryTest extends \PHPUnit_Framework_TestCase
         $session_factory = new SessionFactory();
         $session = $session_factory->newInstance($_COOKIE);
         $this->assertInstanceof('FOA\Auth_Session_Bundle\Session', $session);
-    }
-
-    public function testGetGenerator()
-    {
-        $session_factory = new SessionFactory();
-        $generator = $session_factory->getGenerator();
-        $this->assertInstanceof('RandomLib\Generator', $generator);
-    }
-
-    public function testSetGenerator()
-    {
-        $session_factory = new SessionFactory();
-        $factory = new RandomLibFactory;
-        $generator = $factory->getLowStrengthGenerator();
-        $session_factory->setGenerator($generator);
-        $expected = $session_factory->getGenerator();
-        $this->assertSame($generator, $expected);
     }
 }
